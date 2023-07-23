4826e738a79305db190083cc894ccd9302e76bb6
giladvo24-meet <139681273+giladvo24-meet@users.noreply.github.com>
2023-07-23 18:26:13 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -5,6 +5,15 @@ import pyrebase
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
+config = {
+  "apiKey": "AIzaSyBsWDq5JrfvQpZ7qjzeFHrMK903h_m5eqo",
+  "authDomain": "iasafood.firebaseapp.com",
+  "projectId": "iasafood",
+  "storageBucket": "iasafood.appspot.com",
+  "messagingSenderId": "1092894316925",
+  "appId": "1:1092894316925:web:2216a4fc39458d1c87d414",
+  "measurementId": "G-ZGX9XWSYGS"
+}
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
