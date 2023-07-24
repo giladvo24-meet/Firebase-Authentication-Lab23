a238a73a289d166fe73ee6f01e40fb4051917aa9
giladvo24-meet <139681273+giladvo24-meet@users.noreply.github.com>
2023-07-24 13:20:28 +0300

    done

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -12,16 +12,36 @@ config = {
   "storageBucket": "iasafood.appspot.com",
   "messagingSenderId": "1092894316925",
   "appId": "1:1092894316925:web:2216a4fc39458d1c87d414",
-  "measurementId": "G-ZGX9XWSYGS"
+  "measurementId": "G-ZGX9XWSYGS",
+  "databaseURL": "",
 }
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session["user"] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for("add_tweet"))
+        except:
+            error = "signin failed"
     return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session["user"] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for("add_tweet"))
+        except:
+            error = "signup failed"
     return render_template("signup.html")
 
 
